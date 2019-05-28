a352e254274c3db36eba4da84e0318236028c9ad
Blake Williams <blake@blakewilliams.me>
2019-05-27 22:09:34 -0400

    Handle clicked links

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,8 +1,7 @@
 import url from 'url'
 import path from 'path'
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, shell } from 'electron'
 import isElectronDev from 'electron-is-dev'
-import store from './store'
 import Server from './server'
 import SlackCommands from './slack-commands'
 
@@ -14,19 +13,27 @@ server.connect().then(() => {
 let mainWindow: undefined | BrowserWindow
 
 app.on('ready', () => {
-  setTimeout(() => {
-    mainWindow = new BrowserWindow({
-      titleBarStyle: 'hidden',
-      height: 600,
-      width: 800,
-      webPreferences: {
-        nodeIntegration: true,
-      },
-    })
+  mainWindow = new BrowserWindow({
+    titleBarStyle: 'hidden',
+    height: 600,
+    width: 800,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  })
 
-    mainWindow.loadURL(getHtmlPath('chat.html'))
-    mainWindow.webContents.openDevTools()
-  }, 3000)
+  mainWindow.loadURL(getHtmlPath('chat.html'))
+  const handleRedirect = (e: any, url: string) => {
+    if (url != mainWindow.webContents.getURL()) {
+      e.preventDefault()
+      shell.openExternal(url)
+    }
+  }
+
+  mainWindow.webContents.on('will-navigate', handleRedirect)
+  mainWindow.webContents.on('new-window', handleRedirect)
+
+  mainWindow.webContents.openDevTools()
 })
 
 app.on('window-all-closed', () => {
